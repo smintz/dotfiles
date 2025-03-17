5d6fe1d453d7cfd9faf8b5e9e237192d7ae2a232
Shahar Mintz <smintz@protoconf.com>
2025-03-17 23:25:37 +0700

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Install Starship
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh > /tmp/starship.sh
+sh /tmp/starship.sh -f
+
+cp .config/starship.toml ~/.config
+cp .aliases .zshrc .bashrc ~
